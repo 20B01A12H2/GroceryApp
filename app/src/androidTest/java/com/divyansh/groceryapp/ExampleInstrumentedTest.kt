63d78b3e57023018778c2806c74d01f409a9bd14
20B01A12H2 <95676411+20B01A12H2@users.noreply.github.com>
2022-09-26 00:03:10 +0530

    Update and rename app/src/androidTest/java/com/lakshmidurga/groceryapp/ExampleInstrumentedTest.kt to app/src/androidTest/java/com/divyansh/groceryapp/ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/divyansh/groceryapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/divyansh/groceryapp/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.lakshmidurga.groceryapp
+package com.divyansh.groceryapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
